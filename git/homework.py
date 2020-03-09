af44793dbeac15e8c6ef6b40776ae2fc181bc39b
ArturYefriemov <daria.mid@ukr.net>
2020-03-10 01:15:32 +0200

    home_work1

--- git/homework.py
+++ git/homework.py
@@ -18,7 +18,7 @@ def is_two_object_has_same_value(first: Any, second: Any) ->bool:
     return first == second
 
 
-def is_two_objects_has_same_type(first: Any, second: Any) -> bool:
+def is_two_objects_has_same_type(first: Any, second: Any) ->bool:
     """
     If @first and @second has same type should return True
     In another case should return False
